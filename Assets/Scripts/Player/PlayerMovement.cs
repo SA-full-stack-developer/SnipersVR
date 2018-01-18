0694e59eb42a45b9a36c9ca7bb6a1810e8a32ef0
AxelJG <axeljimenez33@gmail.com>
2018-01-18 19:36:11 +0100

    Puntero suelo
    El puntero aparece en el suelo cuando accionamos el stick izquierdo hacia delante.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,78 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class PlayerMovement : MonoBehaviour {
 
+    //CONFIG MANDOS OCULUS
+    public SteamVR_TrackedObject mTrackedObject = null;
+    public SteamVR_Controller.Device mDevice;
+
+    //GameObjects
     public GameObject leftHand;
-    public Transform pointToMoveSprite;
-    public GameObject vrCamera;
-    //public OVRScreenFade screenFadeScript;
-    public GameObject parentCamera;
-    
-    private bool moving = false;
-    private Vector3 punto;
+    private Transform pointToMoveSprite;
+
+    //RAYCAST
     private RaycastHit hitFloor;
     private Vector3 fwd;
 
-    private void Start()
-    {
+    private void Awake() {
+        mTrackedObject = GetComponent<SteamVR_TrackedObject>();
+        pointToMoveSprite = GameObject.Find("crosshair_go").transform;
     }
 
-    // Update is called once per frame
-    void Update() {
-        //Rayo de movimiento
-        rayCastFloor();
+    private void Update() {
+        mDevice = SteamVR_Controller.Input((int)mTrackedObject.index);
 
-        //Accion de moverse
-        if (moving)
-        {
-            move();
-        }
-    }
+        fwd = leftHand.transform.TransformDirection(Vector3.forward); 
 
-    private void rayCastFloor()
-    {
-        //Preparamos colision y direccion del rayo
-        fwd = leftHand.transform.TransformDirection(Vector3.forward);
+        //Stick hacia delante y colisiona con algo
+        if (mDevice.GetAxis().y > 0.5f && Physics.Raycast(leftHand.transform.position, fwd, out hitFloor, 50)) {
+            if (hitFloor.collider.CompareTag("Floor")) { //Ese algo es el suelo
+                if (!pointToMoveSprite.GetComponent<SpriteRenderer>().enabled) //Si no se ve indicador
+                    pointToMoveSprite.GetComponent<SpriteRenderer>().enabled = true; // Se activa
 
-        /*if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) > 0 
-            && Physics.Raycast(leftHand.transform.position, fwd, out hitFloor, 50) //Si colisiona con algo
-            && hitFloor.collider.CompareTag("Floor"))
-        {//Si es el suelo
-            if (!pointToMoveSprite.GetComponent<SpriteRenderer>().enabled) //Si no se ve indicador
-                pointToMoveSprite.GetComponent<SpriteRenderer>().enabled = true; // Se activa
-
-            pointToMoveSprite.position = new Vector3(hitFloor.point.x, hitFloor.point.y + 0.1f, hitFloor.point.z);
-
-            //Si se pulsa se inicia movimiento
-            if (OVRInput.Get(OVRInput.Button.Four) == true && moving == false)
-            {
-                vrCamera.transform.parent = null;
-                transform.LookAt(new Vector3(hitFloor.point.x, transform.position.y, hitFloor.point.z));
-                punto = new Vector3(hitFloor.point.x, transform.position.y, hitFloor.point.z);
-                moving = true;
+                pointToMoveSprite.position = new Vector3(hitFloor.point.x, hitFloor.point.y + 0.1f, hitFloor.point.z);
+            } else { //Si no choca contra el suelo
+                if (pointToMoveSprite.GetComponent<SpriteRenderer>().enabled) //Si esta activo el indicador
+                    pointToMoveSprite.GetComponent<SpriteRenderer>().enabled = false;//se desactiva
             }
-        }
-        else
-        { //Si no choca contra el suelo
-            if (pointToMoveSprite.GetComponent<SpriteRenderer>().enabled) //Si esta activo el indicador
-                pointToMoveSprite.GetComponent<SpriteRenderer>().enabled = false;//se desactiva
-        }*/
-    }
-
-    private void move()
-    {
-        float step = 1 * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, punto, step);
-
-        if (transform.position.x == punto.x && transform.position.z == punto.z)
-        {
-            moving = false;
-            //screenFadeScript.VisionOut();
-            vrCamera.transform.parent = parentCamera.transform;
-            vrCamera.transform.localPosition = new Vector3(0, 0, -0.07f);
-            vrCamera.transform.localEulerAngles = transform.forward;
+        } else {
+            pointToMoveSprite.GetComponent<SpriteRenderer>().enabled = false;//se desactiva
         }
     }
 }
